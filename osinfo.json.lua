3e9783ed10d85c5958007a3b507fda88e9ab7398
TunayAdaKaracan <tunayada@gmail.com>
2024-11-25 20:38:16 +0100

    OSExplorer Integration

---
+++ osinfo.json.lua
@@ -0,0 +1,16 @@
+{
+    author = {discord = "@kutuptilkisi", github = "https://github.com/TunayAdaKaracan", name = "KutupTilkisi"},
+    description = "A linux like OS for your Picotron",
+    id = "foxos",
+    name = "FoxOS",
+    versions = {
+        {
+            easy_installer = true,
+            entrypoint = "sysboot.lua",
+            installer = "https://raw.githubusercontent.com/TunayAdaKaracan/picotron-foxos/refs/heads/main/installer.easy.json",
+            picotron_build = "=12",
+            tags = {"Alpha"},
+            version = "0.0.1"
+        }
+    }
+}
